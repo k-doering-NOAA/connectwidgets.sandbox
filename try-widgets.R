6be3561024416b772e8df3ad2998e9d5c9a1adb9
k-doering-noaa <kathryn.doering@noaa.gov>
2022-11-21 15:12:07 -0500

    add files to test out widgets

---
+++ try-widgets.R
@@ -0,0 +1,7 @@
+# try out connect widgets package
+# install.packages("connectwidgets")
+
+# rmarkdown::draft("example-page.Rmd", template = "connectwidgets", 
+# package = "connectwidgets")
+
+# rsconnect::writeManifest()
